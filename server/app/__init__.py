dc0bcc4ad84c84364a759f202d02fb4bc5738e4e
hyoaru <jjcabreraaaa@gmail.com>
2025-02-07 18:33:58 +0800

    [add] cors

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
 from app.api.routers.recipes import router as router_recipes
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app():
     load_dotenv()
+
     app = FastAPI(docs_url="/")
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            "http://localhost:8002",
+            "https://hyoaru.github.io/recipe-suggester-ai",
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     app.include_router(
         router_recipes,
         prefix="/api",
